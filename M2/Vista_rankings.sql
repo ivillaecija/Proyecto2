c5e58ee8bda159f2bf179119cba2f9330f8f8a25
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-05 18:43:30 +0200

    Add files via upload

--- M2/Vista_rankings.sql
+++ M2/Vista_rankings.sql
@@ -1,11 +1,8 @@
--- ENEMICS DERROTATS
-create view enemics_derrotats as 
-	select id_jugador, count(*) as enemics_derrotats from Batalla 
-    where dany_realitzat > dany_rebut group by id_jugador;
-    
--- RANKINGS
-create view rankings as select b.id_jugador, sum(b.puntuacio) as puntuacio_global, e.enemics_derrotats,
-sum(b.dany_realitzat) as dany_realitzat, sum(b.dany_rebut) as dany_rebut
-from Batalla b join enemics_derrotats e on e.id_jugador = b.id_jugador group by id_jugador;
+create view rankings as 
+select j.nom, sum(b.puntuacio) as puntuacio_global, 
+	(select count(*) from Batalla where dany_realitzat > dany_rebut 
+    and id_jugador = j.id_jugador) as enemics_derrotats,  sum(b.dany_realitzat) as dany_realitzat,
+    sum(b.dany_rebut) as dany_rebut from Jugador j 
+join Batalla b on b.id_jugador = j.id_jugador
+group by j.id_jugador;
 
-    

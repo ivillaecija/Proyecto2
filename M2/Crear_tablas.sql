6cb907238f24fe7e381165ed6dc31aa4733dfb3d
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-05 19:29:24 +0200

    Add files via upload

---
+++ M2/Crear_tablas.sql
@@ -0,0 +1,46 @@
+create database if not exists battle;
+use battle;
+
+-- TAULES DE CONFIGURACIÓ
+create table weapons (
+	weapon_id int unsigned primary key,
+    weapon_name varchar(30) not null,
+    weapon_image_path varchar(30)
+);
+
+create table warriors (
+	warrior_id int unsigned primary key,
+    warrior_name varchar(30) not null,
+	warrior_image_path varchar(30)
+);
+
+create table players (
+	player_id int unsigned primary key,
+    player_name varchar(30) not null
+);
+
+create table weapons_available (
+	warrior_id int unsigned,
+    weapon_id int unsigned,
+    constraint warrior_id foreign key (warrior_id) references warriors(warrior_id),
+    constraint weapon_id foreign key (weapon_id) references weapons(weapon_id)
+);
+
+
+-- TAULES DE BATALLA
+create table battle (
+	battle_id int unsigned primary key,
+    player_id int unsigned not null,
+    warrior_id int unsigned not null,
+    warrior_weapon_id int unsigned not null,
+	opponent_id int unsigned not null,
+    opponent_weapon_id int unsigned not null,
+	injuries_caused int not null,
+    injuries_suffered int not null,
+    battle_points int not null,
+    foreign key (player_id) references players(player_id),
+    foreign key (warrior_id) references warriors(warrior_id),
+    foreign key (warrior_weapon_id) references weapons(weapon_id),
+    foreign key (opponent_id) references warriors(warrior_id),
+    foreign key (opponent_weapon_id) references weapons(weapon_id)
+);	

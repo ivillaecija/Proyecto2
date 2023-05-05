2ae39733cdaf7dbc1425fb44b10b9e752805cfea
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-05 19:54:53 +0200

    Add files via upload

---
+++ M2/Datos_ejemplo.sql
@@ -0,0 +1,17 @@
+-- ESTO SÓN DATOS INVENTADOS DE PRUEBA
+
+delete from players;
+
+insert into players values(1, 'Izan');
+insert into players values(2, 'Joel');
+insert into players values(3, 'Bryan');
+
+insert into battle values(1, 1, 1, 1, 2, 1, 40, 30, 100);
+insert into battle values(2, 2, 1, 2, 1, 1, 30, 20, 103);
+insert into battle values(3, 3, 1, 4, 2, 7, 30, 40, 230);
+insert into battle values(4, 1, 2, 2, 3, 9, 30, 10, 86);
+insert into battle values(5, 2, 2, 7, 1, 1, 40, 15, 71);
+insert into battle values(6, 3, 2, 8, 3, 8, 10, 20, 45);
+insert into battle values(7, 1, 3, 9, 1, 1, 12, 30, 130);
+insert into battle values(8, 2, 3, 8, 2, 7, 39, 35, 240);
+insert into battle values(9, 3, 3, 2, 1, 1, 30, 10, 12);

2782f979e6309f1a15adb464b71e8ca28423c16b
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-09 20:16:16 +0200

    Add rounds table

--- M2/Crear_tablas.sql
+++ M2/Crear_tablas.sql
@@ -3,24 +3,24 @@ use battle;
 
 -- TAULES DE CONFIGURACIÓ
 create table weapons (
-	weapon_id int unsigned primary key,
+    weapon_id int unsigned primary key,
     weapon_name varchar(30) not null,
     weapon_image_path varchar(30)
 );
 
 create table warriors (
-	warrior_id int unsigned primary key,
+    warrior_id int unsigned primary key,
     warrior_name varchar(30) not null,
-	warrior_image_path varchar(30)
+    warrior_image_path varchar(30)
 );
 
 create table players (
-	player_id int unsigned primary key,
+    player_id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     player_name varchar(30) not null
 );
 
 create table weapons_available (
-	warrior_id int unsigned,
+    warrior_id int unsigned,
     weapon_id int unsigned,
     constraint warrior_id foreign key (warrior_id) references warriors(warrior_id),
     constraint weapon_id foreign key (weapon_id) references weapons(weapon_id)
@@ -29,13 +29,13 @@ create table weapons_available (
 
 -- TAULES DE BATALLA
 create table battle (
-	battle_id int unsigned primary key,
+    battle_id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     player_id int unsigned not null,
     warrior_id int unsigned not null,
     warrior_weapon_id int unsigned not null,
-	opponent_id int unsigned not null,
+    opponent_id int unsigned not null,
     opponent_weapon_id int unsigned not null,
-	injuries_caused int not null,
+    injuries_caused int not null,
     injuries_suffered int not null,
     battle_points int not null,
     foreign key (player_id) references players(player_id),
@@ -43,4 +43,23 @@ create table battle (
     foreign key (warrior_weapon_id) references weapons(weapon_id),
     foreign key (opponent_id) references warriors(warrior_id),
     foreign key (opponent_weapon_id) references weapons(weapon_id)
-);	
+);
+
+create table BATTLE_ROUNDS(
+	battle_rounds_id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	battle_id unisegned not null,
+	player_id int unsigned not null,
+	warrior_id int unsigned not null,
+	warrior_weapon_id int unsigned not null,
+	opponent_id int unsigned not null,
+	opponent_weapon_id int unsigned not null,
+	injuries_caused int not null,
+	injuries_suffered int not null,
+
+	foreing key (battle_id) references battle(battle_id)
+	foreign key (player_id) references players(player_id),
+	foreign key (warrior_id) references warriors(warrior_id),
+	foreign key (warrior_weapon_id) references weapons(weapon_id),
+	foreign key (opponent_id) references warriors(warrior_id),
+	foreign key (opponent_weapon_id) references weapons(weapon_id)
+)

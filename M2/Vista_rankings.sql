724b90b7d13a5590c3df02373396ca7ed540c2b5
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-09 18:36:38 +0200

    Add files via upload

--- M2/Vista_rankings.sql
+++ M2/Vista_rankings.sql
@@ -1,8 +1,7 @@
 create view rankings as 
-select j.nom, sum(b.puntuacio) as puntuacio_global, 
-	(select count(*) from Batalla where dany_realitzat > dany_rebut 
-    and id_jugador = j.id_jugador) as enemics_derrotats,  sum(b.dany_realitzat) as dany_realitzat,
-    sum(b.dany_rebut) as dany_rebut from Jugador j 
-join Batalla b on b.id_jugador = j.id_jugador
-group by j.id_jugador;
-
+select p.player_name, sum(b.battle_points) as global_points, 
+	(select count(*) from battle where injuries_caused > injuries_suffered and
+	player_id = p.player_id) as defeated_enemies, sum(b.injuries_caused) as injuries_caused,
+    sum(b.injuries_suffered) as injuries_suffered from players p
+join battle b on p.player_id = b.player_id
+group by p.player_id;

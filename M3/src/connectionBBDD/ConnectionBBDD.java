6b37cb1aaa005b857d562916cf8f735ffff5a709
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-17 16:26:28 +0200

    Add files via upload

--- M3/src/connectionBBDD/ConnectionBBDD.java
+++ M3/src/connectionBBDD/ConnectionBBDD.java
@@ -2,6 +2,8 @@ package connectionBBDD;
 
 import armas.*;
 import crearPersonaje.WeaponContainer;
+import rankings.*;
+import usuario.*;
 import warriors.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,7 +20,7 @@ public class ConnectionBBDD {
 	public ConnectionBBDD() {
 		String url = "jdbc:mysql://localhost/battle?serverTimezone=UTC";
 		String user = "root";
-		String password = "juan";
+		String password = "1234";
 
 		try {
 			// Buscamos si existe el driver
@@ -38,6 +40,7 @@ public class ConnectionBBDD {
 		}
 
 	}
+
 
 	public ArrayList<Warrior> showWarriors() {
 		ArrayList<Warrior> list = new ArrayList<Warrior>();
@@ -171,9 +174,6 @@ public class ConnectionBBDD {
 
 		ArrayList<Weapon> listaArmas = listaArmas2.WeaponContainer();
 
-		for (Weapon arm : listaArmas) {
-			System.out.println(arm.toString());
-		}
 
 		ArrayList<Weapon> armasCompatibles = new ArrayList<>();
 		ArrayList<Integer> armas = new ArrayList<>();
@@ -201,4 +201,258 @@ public class ConnectionBBDD {
 		}
 		return armasCompatibles;
 	}
+	
+	
+	public void insertarBattleInicio ( Warrior enemigo, Usuario usuario) {
+		try {
+			String query = "INSERT INTO BATTLE VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, recuperarMaxIdBattle()+1);
+			ps.setInt(2, usuario.getId());
+			ps.setInt(3, usuario.getWarrior().getId());
+			ps.setInt(4, usuario.getWarrior().getArma().getId());
+			ps.setInt(5, enemigo.getId());
+			ps.setInt(6, enemigo.getArma().getId());
+			ps.setInt(7, 0);
+			ps.setInt(8, 0);
+			ps.setInt(9, 0);
+			int filasAfectadas = ps.executeUpdate();
+			if (filasAfectadas > 0) {
+	            System.out.println("Se ha insertado el registro correctamente.");
+	        } else {
+	            System.out.println("No se ha insertado ningun registro.");
+	        }
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	public int recuperarMaxIdBattle() {
+		int maxId = 1;
+		try {
+			String query = "SELECT MAX(battle_id) from battle";
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery(query);
+			if (rs.next()) {
+				 maxId = rs.getInt(1);
+			}
+			return maxId;
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+		return maxId;
+	}
+	
+	public void insertarDatosRonda(int danyRealizado, int danyRecibido, Usuario usuario, Usuario enemigo) {
+		int battle_rounds_id = 1;
+		try {
+			String query = "select count(*) from battle_rounds where battle_id = " + recuperarMaxIdBattle() + ";";
+			Statement stmnt = con.createStatement();
+			ResultSet rs = stmnt.executeQuery(query);
+			rs.next();
+			
+			if (rs.getInt(1) > 0) {
+				try {
+					query = "select max(battle_rounds_id) from battle_rounds where battle_id = " + recuperarMaxIdBattle() + ";";
+					rs = stmnt.executeQuery(query);
+					rs.next();
+					battle_rounds_id += rs.getInt(1);
+				} catch (SQLException e1) {
+					System.out.println("No se han encontrado rondas con el battle_id especificado");
+					e1.printStackTrace();
+				}
+			}
+			query = "INSERT INTO BATTLE_ROUNDS VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, battle_rounds_id);
+			ps.setInt(2, recuperarMaxIdBattle());
+			ps.setInt(3, usuario.getId());
+			ps.setInt(4, usuario.getWarrior().getId());
+			ps.setInt(5, usuario.getWarrior().getArma().getId());
+			ps.setInt(6, enemigo.getWarrior().getId());
+			ps.setInt(7, enemigo.getWeapon().getId());
+			ps.setInt(8, danyRealizado);
+			ps.setInt(9, danyRecibido);
+			int filasAfectadas = ps.executeUpdate();
+			if (filasAfectadas > 0) {
+	            System.out.println("Se ha insertado el registro correctamente.");
+	        } else {
+	            System.out.println("No se ha insertado ningun registro.");
+	        }
+			ps.close();
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public void actualizarDatosBattle(int danyRealizadoTotal, int danyRecibidoTotal, int puntos) {
+		try {
+		    String query = "UPDATE battle SET injuries_caused=?, injuries_suffered=?, battle_points=? WHERE battle_id=?;";
+		    PreparedStatement ps = con.prepareStatement(query);
+		    ps.setInt(1, danyRealizadoTotal);
+		    ps.setInt(2, danyRecibidoTotal);
+		    ps.setInt(3, puntos);
+		    ps.setInt(4, recuperarMaxIdBattle());
+		    int filasAfectadas = ps.executeUpdate();
+		    if (filasAfectadas > 0) {
+		        System.out.println("Se ha actualizado la fila correctamente.");
+		    } else {
+		        System.out.println("No se ha actualizado ninguna fila.");
+		    }
+		} catch (SQLException e) {
+		    System.out.println("No se ha podido actualizar la fila: " + e.getMessage());
+		}
+
+	}
+	
+	public void borrarUsuarioDeBattle(Usuario usuario) {
+		try {
+			String query = "DELETE FROM battle where player_id = ?;";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, usuario.getId());
+			int filasAfectadas = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	public void borrarBattleRounds(Usuario usuario){
+		try {
+			String query = "DELETE FROM battle_rounds where player_id = ?;";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, usuario.getId());
+			int filasAfectadas = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	public void borrarPartidaIncompleta(Usuario usuario) {
+		try {
+			String query1 = "select battle_id from battle where player_id=? and injuries_caused = 0 and injuries_suffered = 0;";
+
+			PreparedStatement ps = con.prepareStatement(query1);
+
+			// Establecer el valor para el marcador de posición
+			ps.setInt(1, usuario.getId());
+
+			// Ejecutar la consulta
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				
+				System.out.println("entro");
+				int battleId = rs.getInt(1);
+				borrar(battleId);
+				
+			}
+
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	public void borrar(int battleId) {
+		try {
+			String query2 = "DELETE FROM battle_rounds where battle_id = ?;";
+			PreparedStatement ps = con.prepareStatement(query2);
+			ps.setInt(1, battleId);
+			ps.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+		}
+
+	}
+	
+	
+
+	
+	public void borrarUsuario(Usuario usuario) {
+		try {
+			String query = "DELETE FROM players where player_id = ?";
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.setInt(1, usuario.getId());
+			int filasAfectadas = ps.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Ranking> rankings() {
+		ArrayList<Ranking> a_rankings = new ArrayList<Ranking>();
+		try {
+			String query = "select * from rankings;";
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(query);
+
+			while (rs.next()) {
+				a_rankings.add(new Ranking(rs.getString(1),rs.getInt(2),rs.getInt(3),rs.getInt(4),rs.getInt(5)));
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("No se ha podido crear la conexion");
+		}
+		finally {
+			return a_rankings;
+		}
+	}
+	
+	public int insertarUsuario(String s) {
+		String query = "select count(*) from players where player_name like '" + s + "';";
+		int player_id = 1;
+		try {
+			Statement stmnt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs = stmnt.executeQuery(query);
+			rs.next();
+			int contador = rs.getInt(1);
+			if (contador == 0) {
+				query = "select max(player_id) from players;";
+				try {
+					rs = stmnt.executeQuery(query);
+					rs.next();
+					player_id += rs.getInt(1);
+				}
+				catch (SQLException e1) {
+					System.out.println("No se ha podido encontrar usuarios");
+					e1.printStackTrace();
+				}
+				finally {
+					query = "select * from players;";
+					rs = stmnt.executeQuery(query);
+					rs.last();
+					rs.moveToInsertRow();
+					rs.updateInt(1, player_id);
+					rs.updateString(2, s);
+					rs.insertRow();
+				}
+			}
+			else {
+				query = "select player_id from players where player_name like '" + s + "';";
+				rs = stmnt.executeQuery(query);
+				rs.next();
+				player_id = rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("No se ha podido insertar usuario");
+			e.printStackTrace();
+		}
+		return player_id;
+	}
+	
+	
 }

407b6beeb45f4d4377bbaba4925ee6ccd36e0ac8
ivillaecija <118919673+ivillaecija@users.noreply.github.com>
2023-05-17 17:00:11 +0200

    Add files via upload

--- M3/src/usuario/Usuario.java
+++ M3/src/usuario/Usuario.java
@@ -6,8 +6,31 @@ import warriors.*;
 public class Usuario {
 	private Warrior warrior;
 	private Weapon weapon;
-	private int puntos;
+	private int puntos = 0;
+	private String nombre = "";
+	private int id;
+	private int partidasJugadas = 0;
+	private boolean gameOver = false;
 	
+	
+	public boolean getGameOver() {
+		return gameOver;
+	}
+	public void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+	public int getPartidasJugadas() {
+		return partidasJugadas;
+	}
+	public void setPartidasJugadas(int partidasJugadas) {
+		this.partidasJugadas = partidasJugadas;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 	public Usuario() {
 	}
 	public Warrior getWarrior() {
@@ -25,12 +48,20 @@ public class Usuario {
 	public int getPuntos() {
 		return puntos;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public void setPuntos(int puntos) {
 		this.puntos = puntos;
 	}
+	
 	@Override
 	public String toString() {
-		return "Usuario [warrior=" + warrior + ", weapon=" + weapon + ", puntos=" + puntos + "]";
+		return "Usuario [warrior=" + warrior + ", weapon=" + weapon + ", puntos=" + puntos + ", nombre=" + nombre
+				+ ", id=" + id + ", partidasJugadas=" + partidasJugadas + "]";
 	}
 	
 	
